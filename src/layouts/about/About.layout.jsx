5cb6f0bd0126d85fa266bd65f74666159bcf7976
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-07-20 18:56:51 +0530

    Update About.layout.jsx

--- src/layouts/about/About.layout.jsx
+++ src/layouts/about/About.layout.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import AboutSection from "../../components/about-section/AboutSection.component";
+import YoutubeSection from "../../components/youtube-section/YoutubeSection.component";
 
 const About = () => {
   return (
@@ -8,6 +9,7 @@ const About = () => {
       <Outlet />
       <div className="mr-2 ml-2 mt-5 mb-5">
         <AboutSection />
+        <YoutubeSection/>
       </div>
     </Fragment>
   );
