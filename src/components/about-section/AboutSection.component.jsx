f97d10df708dcfba9c8fb43b8676036bfeec3fb6
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-07-20 18:56:10 +0530

    Update AboutSection.component.jsx

--- src/components/about-section/AboutSection.component.jsx
+++ src/components/about-section/AboutSection.component.jsx
@@ -8,7 +8,8 @@ const AboutSection = () => {
         </div>
         <div className='col-lg-6 col-sm-10 mt-lg-5 mt-sm-0 mb-5'>
             <div className='row justify-content-center ml-sm-0 ml-lg-3 mr-sm-0 mr-lg-3 mt-3 mb-3'>
-                <h2>About Us</h2>
+                <h2>ABOUT US</h2>
+                <hr/>
                 <h4 className='mb-3'>Lorem Ipsum is simply dummy text</h4>
                 <p className='p-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
                     standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
