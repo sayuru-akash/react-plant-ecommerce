27728773fe6fdfab0181537a5a8726a247d802e7
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-08-07 12:26:24 +0530

    add cart context

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 import { UserContext, UserProvider } from "./context/user.context";
+import { CartContext,CartProvider } from "./context/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <CartProvider>
+          <App />
+        </CartProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
