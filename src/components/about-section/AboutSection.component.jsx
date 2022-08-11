c9fa80036c714de7d90b9c04754bec2e7c744524
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-08-11 16:27:59 +0530

    replace dummy txt

--- src/components/about-section/AboutSection.component.jsx
+++ src/components/about-section/AboutSection.component.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const AboutSection = () => {
   return (
-    <div className='row  m-lg-5 m-sm-1'>
-        <div className='col-lg-6 col-sm-12 mt-5 mb-lg-5 mb-sm-3'>
-            <img className='img-fluid w-75' src="https://i.ibb.co/mvBDXnG/nikola-jovanovic-OBok3-F8bu-KY-unsplash-1.jpg" alt="about image"/>
+    <div className="row  m-lg-5 m-sm-1">
+      <div className="col-lg-6 col-sm-12 mt-5 mb-lg-5 mb-sm-3">
+        <img
+          className="img-fluid w-75"
+          src="https://i.ibb.co/mvBDXnG/nikola-jovanovic-OBok3-F8bu-KY-unsplash-1.jpg"
+          alt="about image"
+        />
+      </div>
+      <div className="col-lg-6 col-sm-10 mt-lg-5 mt-sm-0 mb-5">
+        <div className="row justify-content-center ml-sm-0 ml-lg-3 mr-sm-0 mr-lg-3 mt-3 mb-3">
+          <h2>ABOUT US</h2>
+          <hr />
+          <h4 className="mb-3">
+            We are a small scale indoor plant growers in Kandy, Sri Lanka.
+          </h4>
+          <p className="p-4">
+            In our plant nursery, we nurture an excellent collection of high
+            quality indoor and fruit plants for sale. We believe associating us
+            with natural plants can bring a peace and mindfulness to your busy
+            lives. Taking care of a plant and watching it flourish is kind of a
+            meditation itself
+          </p>
         </div>
-        <div className='col-lg-6 col-sm-10 mt-lg-5 mt-sm-0 mb-5'>
-            <div className='row justify-content-center ml-sm-0 ml-lg-3 mr-sm-0 mr-lg-3 mt-3 mb-3'>
-                <h2>ABOUT US</h2>
-                <hr/>
-                <h4 className='mb-3'>Lorem Ipsum is simply dummy text</h4>
-                <p className='p-4'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-                    make a type specimen book. It has Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                    standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-                    make a type specimen book. It has</p>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- src/components/footer/Footer.component.jsx
+++ src/components/footer/Footer.component.jsx
@@ -1,91 +1,119 @@
-import React from 'react'
-import './Footer.styles.css'
+import React from "react";
+import "./Footer.styles.css";
 const Footer = () => {
   return (
     <footer className="footer text-center text-lg-start bg-light text-muted mt-4">
-    <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
         <div className="me-5 d-none d-lg-block">
-        <span>Get connected with us on social networks:</span>
+          <span>Get connected with us on social networks:</span>
         </div>
         <div>
-        <a href="" className="me-4 text-reset">
+          <a href="" className="me-4 text-reset">
             <i className="fab fa-facebook-f"></i>
-        </a>
-        <a href="" className="me-4 text-reset">
+          </a>
+          <a href="" className="me-4 text-reset">
             <i className="fab fa-twitter"></i>
-        </a>
-        <a href="" className="me-4 text-reset">
+          </a>
+          <a href="" className="me-4 text-reset">
             <i className="fab fa-google"></i>
-        </a>
+          </a>
         </div>
-    </section>
-    <section className="">
+      </section>
+      <section className="">
         <div className="container text-center text-md-start mt-5">
-        <div className="row mt-3">
+          <div className="row mt-3">
             <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <div className=" mb-4">
-                <img src="https://i.ibb.co/Vgx9sq8/fancy-hut-logo.png" alt="logo" className="nav-logo"/>
-            </div>
-            <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum
-                dolor sit amet, consectetur adipisicing elit.
-            </p>
+              <div className=" mb-4">
+                <img
+                  src="https://i.ibb.co/Vgx9sq8/fancy-hut-logo.png"
+                  alt="logo"
+                  className="nav-logo"
+                />
+              </div>
+              <p>
+                We offer our ultimate gratitude towards this amazing nature by
+                providing the best agricultural plants in order sustain a
+                greener future.
+              </p>
             </div>
             <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 className="text-uppercase fw-bold mb-4">
-                Products
-            </h6>
-            <p>
-                <a href="/about" className="text-reset">About Us</a>
-            </p>
-            <p>
-                <a href="#!" className="text-reset">Shop</a>
-            </p>
-            <p>
-                <a href="#!" className="text-reset">Blog</a>
-            </p>
-            <p>
-                <a href="/contact" className="text-reset">Contact Us</a>
-            </p>
+              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+              <p>
+                <a href="/about" className="text-reset">
+                  About Us
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Shop
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Blog
+                </a>
+              </p>
+              <p>
+                <a href="/contact" className="text-reset">
+                  Contact Us
+                </a>
+              </p>
             </div>
             <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 className="text-uppercase fw-bold mb-4">
-                Useful links
-            </h6>
-            <p>
-                <a href="#!" className="text-reset">Pricing</a>
-            </p>
-            <p>
-                <a href="#!" className="text-reset">Settings</a>
-            </p>
-            <p>
-                <a href="#!" className="text-reset">Orders</a>
-            </p>
-            <p>
-                <a href="#!" className="text-reset">Help</a>
-            </p>
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Help
+                </a>
+              </p>
             </div>
             <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <h6 className="text-uppercase fw-bold mb-4">
-                Contact
-            </h6>
-            <p><i className="fas fa-home me-3"></i> 23/A, Pilligalla Road,<br />Angunawela, Peradeniya,<br/>Kandy, Sri Lanka</p>
-            <p>
+              <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+              <p>
+                <i className="fas fa-home me-3"></i> 23/A, Pilligalla Road,
+                <br />
+                Angunawela, Peradeniya,
+                <br />
+                Kandy, Sri Lanka
+              </p>
+              <p>
                 <i className="fas fa-envelope me-3"></i>
                 fancyhutplants@gmail.com
-            </p>
-            <p><i className="fas fa-phone me-3"></i> + 94 78 533 7984</p>
-            <p><i className="fas fa-phone me-3"></i> + 94 75 506 5056</p>
+              </p>
+              <p>
+                <i className="fas fa-phone me-3"></i> + 94 78 533 7984
+              </p>
+              <p>
+                <i className="fas fa-phone me-3"></i> + 94 75 506 5056
+              </p>
             </div>
+          </div>
         </div>
-        </div>
-    </section>
-    <div className="text-center p-4">
+      </section>
+      <div className="text-center p-4">
         © 2021 Copyright:
-        <a className="text-reset fw-bold" href="/"> Fancy Hut Plants</a>
-    </div>
+        <a className="text-reset fw-bold" href="/">
+          {" "}
+          Fancy Hut Plants
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
