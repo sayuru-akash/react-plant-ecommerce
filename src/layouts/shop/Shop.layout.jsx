9c0b0f0d4a150924e1ae7fdf8ae685c3f375d462
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-08-11 17:25:10 +0530

    Update Shop.layout.jsx

--- src/layouts/shop/Shop.layout.jsx
+++ src/layouts/shop/Shop.layout.jsx
@@ -1,10 +1,20 @@
+import { useSearchParams } from "react-router-dom";
+
 import ProductLoop from "../../components/product-loop/ProductLoop.component"
 
 const Shop = () => {
+
+  const [searchParams] = useSearchParams();
+  const sKey = (searchParams.get('search'));
+  
   return (
+    <>
+    {sKey != null ? <h2 className="mt-4">Search Results for {sKey}</h2> : <h2 className="mt-4">Shop</h2>}
+    
     <div className="row">
       <ProductLoop/>
     </div>
+    </>
   )
 }
 
