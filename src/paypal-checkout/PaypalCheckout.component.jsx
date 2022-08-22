bc62eb3b13caafe5f5992d1ed6560674de1847b6
Isuru Pathirathna <isuru2002@gmail.com>
2022-08-23 01:06:00 +0530

    charts and paypal issue

--- src/paypal-checkout/PaypalCheckout.component.jsx
+++ src/paypal-checkout/PaypalCheckout.component.jsx
@@ -1,36 +1,57 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { useState } from "react";
 
 function PaypalCheckout() {
-  return (
-    <PayPalScriptProvider
-      options={{
-        "client-id":
-          "AY9WWCScGbKNIeGiVFkmAypyoaTCfZWYh5mNdZYwkYr9T5gWnnCeekqEihvXgY37i0rMibvXiU9BK4f8",
-      }}
-    >
-      <PayPalButtons
-        createOrder={(data, actions) => {
-          console.log(data);
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: "1",
-                },
-              },
-            ],
-          });
-        }}
-        onApprove={(data, actions) => {
-            return actions.order.capture().then((details) => {
-              // const name = details.payer.name.given_name;
-              alert(`Transaction completed`);
+ const [success, setSuccess] = useState(false);
+ const [ErrorMessage, setErrorMessage] = useState("");
+ const [orderID, setOrderID] = useState(false);
+ 
+ const createOrder = (data, actions) => {
+   return actions.order
+     .create({
+       purchase_units: [
+         {
+           description: "Sunflower",
+           amount: {
+             currency_code: "USD",
+             value: 2,
+           },
+         },
+       ],
+       application_context: {
+         shipping_preference: "NO_SHIPPING",
+       },
+     })
+     .then((orderID) => {
+       setOrderID(orderID);
+       return orderID;
+     });
+ };
+ 
+ const onApprove = (data, actions) => {
+   return actions.order.capture().then(function (details) {
+     const { payer } = details;
+     setSuccess(true);
+   });
+ };
 
-            });
-        }}
-      />
-    </PayPalScriptProvider>
-  );
+ const onError = (data, actions) => {
+   setErrorMessage("An Error occured with your payment ");
+ };
+ return (
+   <PayPalScriptProvider
+     options={{
+       "client-id":"AY9WWCScGbKNIeGiVFkmAypyoaTCfZWYh5mNdZYwkYr9T5gWnnCeekqEihvXgY37i0rMibvXiU9BK4f8",
+     }}
+   >
+         <PayPalButtons
+           style={{ layout: "vertical" }}
+           createOrder={createOrder}
+           onApprove={onApprove}
+           onError={onError}
+         />
+   </PayPalScriptProvider>
+ );
 }
 
 export default PaypalCheckout;
