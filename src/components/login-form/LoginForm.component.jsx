c62f1d3c339fe1efc095b96f99a8a14a6930e1d2
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2022-08-23 02:30:07 +0530

    set forget password function

--- src/components/login-form/LoginForm.component.jsx
+++ src/components/login-form/LoginForm.component.jsx
@@ -3,18 +3,26 @@ import {
   signInWithGooglePopup,
   signInUserFromEmailAndPassword,
   createUserFromAuth,
+  auth,
 } from "../../utils/firebase/firebaseauth.utils";
 
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { useEffect } from "react";
 
 const defaultFormState = {
   email: "",
   password: "",
 };
 
+const defResetState = {
+  resetMail: "",
+};
+
 const LoginForm = () => {
   const [formState, setFormState] = useState(defaultFormState);
   const { email, password } = formState;
+  const [resetMail, setEmail] = useState(defResetState);
 
   const resetForm = () => {
     setFormState(defaultFormState);
@@ -26,37 +34,47 @@ const LoginForm = () => {
     const response = await signInWithGooglePopup();
     if (response) {
       await createUserFromAuth(response.user);
-      if (response.user.email==="admin@fancyhut.com") {
+      if (response.user.email === "admin@fancyhut.com") {
         navigate("/admin");
-      }else{
+      } else {
         navigate("/dashboard");
       }
+    }
+  };
+
+  const triggerResetEmail = async () => {
+    const pr = await sendPasswordResetEmail(auth, resetMail);
+    if (pr) {
+      alert("PASSWORD RESET mail sent");
+    } else {
+      alert("PASSWORD RESET mail not sent");
     }
   };
 
   const handleSubmit = (event) => {
     const handler = async () => {
       event.preventDefault();
-      if (email === "" || password === "" ) {
+      if (email === "" || password === "") {
         alert("no empty values allowed");
         return;
       }
-      const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-      if(!email || regex.test(email) === false){
-          alert("email is not valid");
-          return;
+      const regex =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      if (!email || regex.test(email) === false) {
+        alert("email is not valid");
+        return;
       }
-      if(password.length<=7){
-          alert("password should have more than 7 characters");
-          return;
+      if (password.length <= 7) {
+        alert("password should have more than 7 characters");
+        return;
       }
       try {
         const { user } = await signInUserFromEmailAndPassword(email, password);
         // setCurrentUser(user);
         resetForm();
-        if (user.email==="admin@fancyhut.com") {
+        if (user.email === "admin@fancyhut.com") {
           navigate("/admin");
-        }else{
+        } else {
           navigate("/dashboard");
         }
       } catch (error) {
@@ -84,104 +102,105 @@ const LoginForm = () => {
 
   return (
     <>
-    <form className="mb-5" onSubmit={handleSubmit}>
-      <div className="mb-5">
-        <h3>LOG IN</h3>
-      </div>
-      <div className="mb-3">
-        <label for="email" className="form-label">
-          Username or email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          onChange={handleChange}
-          name="email"
-          value={email}
-       
-        />
-      </div>
-      <div className="mb-3">
-        <label for="password" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          onChange={handleChange}
-          name="password"
-          value={password}
-        />
-      </div>
-      <button type="submit" className="btn btn-success mt-4 w-100">
-        Sign In
-      </button>
-      <button
-        type="button"
-        className="btn btn-primary mt-4 w-100"
-        onClick={signInWithGooglePU}
-      >
-        Sign In With Google
-      </button>
-      <div className="row justify-content-center">
-      <a
-        className="link-dark mt-4 text-center"
-        data-bs-toggle="modal"
-        data-bs-target="#forgetPasswordModal"
-      >
-        Forget Password
-      </a>
-      </div>
-    </form>
+      <form className="mb-5" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <h3>LOG IN</h3>
+        </div>
+        <div className="mb-3">
+          <label for="email" className="form-label">
+            Username or email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            onChange={handleChange}
+            name="email"
+            value={email}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={handleChange}
+            name="password"
+            value={password}
+          />
+        </div>
+        <button type="submit" className="btn btn-success mt-4 w-100">
+          Sign In
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary mt-4 w-100"
+          onClick={signInWithGooglePU}
+        >
+          Sign In With Google
+        </button>
+        <div className="row justify-content-center">
+          <a
+            className="link-dark mt-4 text-center"
+            data-bs-toggle="modal"
+            data-bs-target="#forgetPasswordModal"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
 
-    <div
-      className="modal fade"
-      id="forgetPasswordModal"
-      tabIndex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog modal-lg">
-        <div className="modal-content">
-          <div className="modal-header text-center">
-            <h5 className="modal-title" id="exampleModalLabel">
-              Forget Password
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <form
-              className="row justify-content-center container"
-            >
-              <div className="mb-3 col-lg-12 col-md-12 col-sm-12">
-                <label htmlFor="email" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  class="form-control"
-                />
-              </div>
-              <div className="row justify-content-center">
-                <button
-                  type="submit"
-                  className="btn btn-warning mt-4 mb-4 w-75"
-                >
-                  Submit
-                </button>
-              </div>
-            </form>
+      <div
+        className="modal fade"
+        id="forgetPasswordModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header text-center">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Forgot Password?
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form className="row justify-content-center container">
+                <div className="mb-3 col-lg-12 col-md-12 col-sm-12">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="resetMail"
+                    onChange={(e) => setEmail(e.target.value)}
+                    name="resetMail"
+                  />
+                </div>
+                <div className="row justify-content-center">
+                  <button
+                    type="button"
+                    className="btn btn-warning mt-4 mb-4 w-75"
+                    onClick={triggerResetEmail}
+                  >
+                    Reset
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
